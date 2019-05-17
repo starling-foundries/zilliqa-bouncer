3704d2e0cf964fc5d0c5d86fa13ee6d09e580e5f
Cameron Sajedi <cameronsajedi@gmail.com>
2019-05-17 16:29:15 -0400

    adding basic .env to manage environment variables

--- src/Server.ts
+++ src/Server.ts
@@ -1,17 +1,18 @@
 import { BN, Long, units, bytes } from "@zilliqa-js/util";
 import { Zilliqa } from "@zilliqa-js/zilliqa";
 import * as CP from "@zilliqa-js/crypto";
-import config from '../config.json';
+// import config from '../config.json';
+import {config} from "dotenv";
 import express from 'express';
 
-
-// API endpoint - change this and CHAIN_ID to use another endpoint than Kaya.
-const zilliqa = new Zilliqa("http://localhost:4200");
+console.log(process.env)
+// API endpoint - change the .env file to use another endpoint than Kaya.
+const zilliqa = new Zilliqa(process.env.API_ENDPOINT);
 
 const app = express();
 
 //Populate the wallet with an account
-const privkey = config.testAccount.privateKey;
+const privkey = process.env.OWNER_PKEY;
 zilliqa.wallet.addByPrivateKey(privkey);
 
 const address = CP.getAddressFromPrivateKey(privkey);
@@ -22,6 +23,8 @@ console.log(`0x${address}`)
 // These numbers are JUST AN EXAMPLE. They will NOT WORK on the public testnet
 // or mainnet. Please check what they are before proceeding, or your
 // transactions will simply be rejected.
-const CHAIN_ID = 111;
-const MSG_VERSION = 1;
+const CHAIN_ID = Number(process.env.CHAIN_ID);
+const MSG_VERSION = Number(process.env.MSG_VERSION);
 const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
+
+
